adf0275c68ca12174fc39edd7e706fa5c007e759
Eipna <Xirveipna@tuta.io>
2025-02-01 11:55:06 +0800

    Added toolbar support in main activity with EdgeToEdge enabled

--- app/src/main/java/com/eipna/centsation/MainActivity.java
+++ app/src/main/java/com/eipna/centsation/MainActivity.java
@@ -1,14 +1,28 @@
 package com.eipna.centsation;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.eipna.centsation.databinding.ActivityMainBinding;
+import com.google.android.material.shape.MaterialShapeDrawable;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Drawable drawable = MaterialShapeDrawable.createWithElevationOverlay(this);
+        binding.appBar.setStatusBarForeground(drawable);
+
+        setSupportActionBar(binding.toolbar);
     }
 }
