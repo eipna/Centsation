f54781f4cf1970cf5bb84959d6abcff99d31a885
Eipna <Xirveipna@tuta.io>
2025-02-01 22:26:08 +0800

    Added bare boned add saving dialog and removed generated ic_launcher_background resource file

--- app/src/main/java/com/eipna/centsation/MainActivity.java
+++ app/src/main/java/com/eipna/centsation/MainActivity.java
@@ -3,10 +3,14 @@ package com.eipna.centsation;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.eipna.centsation.databinding.ActivityMainBinding;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.shape.MaterialShapeDrawable;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,5 +28,18 @@ public class MainActivity extends AppCompatActivity {
         binding.appBar.setStatusBarForeground(drawable);
 
         setSupportActionBar(binding.toolbar);
+
+        binding.addSaving.setOnClickListener(view -> showAddSavingDialog());
+    }
+
+    private void showAddSavingDialog() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this)
+                .setTitle(R.string.dialog_title_add_saving)
+                .setIcon(R.drawable.ic_add)
+                .setNegativeButton(R.string.dialog_button_close, null)
+                .setPositiveButton(R.string.dialog_button_add, null);
+
+        Dialog dialog = builder.create();
+        dialog.show();
     }
 }
