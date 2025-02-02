759ea2206f19e0b687903ac08f624359c1c632d3
Eipna <Xirveipna@tuta.io>
2025-02-02 16:53:54 +0800

    Created database class

---
+++ app/src/main/java/com/eipna/centsation/data/Database.java
@@ -0,0 +1,43 @@
+package com.eipna.centsation.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "centsation.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_SAVING = "savings";
+    public static final String COLUMN_SAVING_ID = "saving_id";
+    public static final String COLUMN_SAVING_NAME = "name";
+    public static final String COLUMN_SAVING_CURRENT_AMOUNT = "current_amount";
+    public static final String COLUMN_SAVING_GOAL = "goal";
+    public static final String COLUMN_SAVING_NOTES = "notes";
+    public static final String COLUMN_SAVING_IS_ARCHIVED = "is_archived";
+
+    public Database(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createSavingTable = "CREATE TABLE IF NOT EXISTS " + TABLE_SAVING + "(" +
+                COLUMN_SAVING_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_SAVING_NAME + " TEXT, " +
+                COLUMN_SAVING_CURRENT_AMOUNT + " REAL, " +
+                COLUMN_SAVING_GOAL + " REAL, " +
+                COLUMN_SAVING_NOTES + " TEXT, " +
+                COLUMN_SAVING_IS_ARCHIVED + " INTEGER)";
+        sqLiteDatabase.execSQL(createSavingTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_SAVING);
+        onCreate(sqLiteDatabase);
+    }
+}
