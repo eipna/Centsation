c3470b7746525709fb29a3c464911196e1f59625
Eipna <Xirveipna@tuta.io>
2025-02-10 20:03:48 +0800

    Created adapter class for saving with basic implementation

---
+++ app/src/main/java/com/eipna/centsation/ui/adapters/SavingAdapter.java
@@ -0,0 +1,77 @@
+package com.eipna.centsation.ui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.eipna.centsation.R;
+import com.eipna.centsation.data.saving.Saving;
+import com.eipna.centsation.data.saving.SavingListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.ArrayList;
+
+public class SavingAdapter extends RecyclerView.Adapter<SavingAdapter.ViewHolder> {
+
+    private final Context context;
+    private final SavingListener savingListener;
+    private ArrayList<Saving> savings;
+
+    public SavingAdapter(Context context, SavingListener savingListener, ArrayList<Saving> savings) {
+        this.context = context;
+        this.savingListener = savingListener;
+        this.savings = savings;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View savingView = LayoutInflater.from(context).inflate(R.layout.recycler_saving, parent, false);
+        return new ViewHolder(savingView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Saving currentSaving = savings.get(position);
+        holder.bind(currentSaving);
+    }
+
+    @Override
+    public int getItemCount() {
+        return savings.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        MaterialTextView name, amount, goal, percent;
+        MaterialButton update, history, archive, delete;
+        Chip copyNotes;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.saving_name);
+            amount = itemView.findViewById(R.id.saving_amount);
+            goal = itemView.findViewById(R.id.saving_goal);
+            percent = itemView.findViewById(R.id.saving_percent);
+
+            update = itemView.findViewById(R.id.saving_update);
+            history = itemView.findViewById(R.id.saving_history);
+            archive = itemView.findViewById(R.id.archive);
+            delete = itemView.findViewById(R.id.saving_delete);
+
+            copyNotes = itemView.findViewById(R.id.saving_copy_notes);
+        }
+
+        public void bind(Saving currentSaving) {
+            name.setText(currentSaving.getName());
+            amount.setText(String.valueOf(currentSaving.getCurrentAmount()));
+            goal.setText(String.valueOf(currentSaving.getGoal()));
+        }
+    }
+}
