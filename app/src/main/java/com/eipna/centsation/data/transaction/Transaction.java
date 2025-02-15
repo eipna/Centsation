f00234cc9e7b89a882df28bee5ffee6fc3e0a4b4
Eipna <Xirveipna@tuta.io>
2025-02-15 12:31:41 +0800

    Created model class for transactions

---
+++ app/src/main/java/com/eipna/centsation/data/transaction/Transaction.java
@@ -0,0 +1,48 @@
+package com.eipna.centsation.data.transaction;
+
+public class Transaction {
+
+    private int ID;
+    private int savingID;
+    private double amount;
+    private String type;
+
+    public Transaction() {
+        this.ID = -1;
+        this.savingID = -1;
+        this.amount = -1;
+        this.type = null;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getSavingID() {
+        return savingID;
+    }
+
+    public void setSavingID(int savingID) {
+        this.savingID = savingID;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
