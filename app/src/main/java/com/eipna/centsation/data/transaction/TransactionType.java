8143f202de492342b044989045c02fdb3681c48a
Eipna <Xirveipna@tuta.io>
2025-02-15 12:47:13 +0800

    Added type value for transaction type enum

--- app/src/main/java/com/eipna/centsation/data/transaction/TransactionType.java
+++ app/src/main/java/com/eipna/centsation/data/transaction/TransactionType.java
@@ -1,12 +1,17 @@
 package com.eipna.centsation.data.transaction;
 
 public enum TransactionType {
-    ADD,
-    DEDUCT;
+    ADD("Add"),
+    DEDUCT("Deduct");
 
     private static final TransactionType[] types;
+    private final String TYPE;
 
     static {
         types = values();
+    }
+
+    TransactionType(String type) {
+        this.TYPE = type;
     }
 }
