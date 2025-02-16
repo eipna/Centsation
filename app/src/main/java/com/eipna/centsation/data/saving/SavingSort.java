054a416f57332648a6e298c4672e49362b17adb8
Eipna <Xirveipna@tuta.io>
2025-02-16 22:39:39 +0800

    Updated saving sort enum

--- app/src/main/java/com/eipna/centsation/data/saving/SavingSort.java
+++ app/src/main/java/com/eipna/centsation/data/saving/SavingSort.java
@@ -1,20 +1,27 @@
 package com.eipna.centsation.data.saving;
 
+
+import java.util.Comparator;
+
 public enum SavingSort {
-    NAME("name"),
-    VALUE("value"),
-    GOAL("goal"),
-    ASCENDING("asc"),
-    DESCENDING("desc");
+
+    NAME_ASCENDING("name_ascending", Saving.SORT_NAME_ASCENDING),
+    NAME_DESCENDING("name_descending", Saving.SORT_NAME_DESCENDING),
+    VALUE_LOWEST("value_lowest", Saving.SORT_VALUE_LOWEST),
+    VALUE_HIGHEST("value_highest", Saving.SORT_VALUE_HIGHEST),
+    GOAL_LOWEST("goal_lowest", Saving.SORT_GOAL_LOWEST),
+    GOAL_HIGHEST("goal_highest", Saving.SORT_GOAL_HIGHEST);
 
     private static final SavingSort[] sorts;
-    public final String SORT;
+    public final String NAME;
+    public final Comparator<Saving> SORT;
 
     static {
         sorts = values();
     }
 
-    SavingSort(String sort) {
+    SavingSort(String name, Comparator<Saving> sort) {
+        this.NAME = name;
         this.SORT = sort;
     }
 }
