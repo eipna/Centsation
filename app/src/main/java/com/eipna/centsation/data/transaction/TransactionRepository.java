f3db795e3351b5eb59210f1b067d5903b930a34b
Eipna <Xirveipna@tuta.io>
2025-02-22 16:22:11 +0800

    Added date attribute for transaction repository

--- app/src/main/java/com/eipna/centsation/data/transaction/TransactionRepository.java
+++ app/src/main/java/com/eipna/centsation/data/transaction/TransactionRepository.java
@@ -23,6 +23,7 @@ public class TransactionRepository extends Database {
         values.put(COLUMN_TRANSACTION_SAVING_ID, createdTransaction.getSavingID());
         values.put(COLUMN_TRANSACTION_AMOUNT, createdTransaction.getAmount());
         values.put(COLUMN_TRANSACTION_TYPE, createdTransaction.getType());
+        values.put(COLUMN_TRANSACTION_DATE, createdTransaction.getDate());
         database.insert(TABLE_TRANSACTION, null, values);
         database.close();
     }
@@ -41,6 +42,7 @@ public class TransactionRepository extends Database {
                 queriedTransaction.setSavingID(cursor.getInt(cursor.getColumnIndex(COLUMN_TRANSACTION_SAVING_ID)));
                 queriedTransaction.setAmount(cursor.getDouble(cursor.getColumnIndex(COLUMN_TRANSACTION_AMOUNT)));
                 queriedTransaction.setType(cursor.getString(cursor.getColumnIndex(COLUMN_TRANSACTION_TYPE)));
+                queriedTransaction.setDate(cursor.getLong(cursor.getColumnIndex(COLUMN_TRANSACTION_DATE)));
                 list.add(queriedTransaction);
             } while (cursor.moveToNext());
         }
