d7b84b937b5b993364ff8841b6a34c9314befb0c
Eipna <Xirveipna@tuta.io>
2025-04-13 08:18:20 +0800

    Updated alarm manager permission to SCHEDULE_EXACT_ALARM

--- app/src/main/java/com/eipna/centsation/ui/activities/EditActivity.java
+++ app/src/main/java/com/eipna/centsation/ui/activities/EditActivity.java
@@ -1,6 +1,8 @@
 package com.eipna.centsation.ui.activities;
 
 import android.Manifest;
+import android.app.AlarmManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -15,6 +17,7 @@ import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -28,6 +31,7 @@ import com.eipna.centsation.util.DateUtil;
 import com.google.android.material.datepicker.CalendarConstraints;
 import com.google.android.material.datepicker.DateValidatorPointForward;
 import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Locale;
@@ -37,6 +41,7 @@ public class EditActivity extends BaseActivity {
 
     private ActivityEditBinding binding;
     private SavingRepository savingRepository;
+    private AlarmManager alarmManager;
 
     private int isArchiveExtra;
     private String IDExtra, nameExtra, notesExtra;
@@ -46,7 +51,7 @@ public class EditActivity extends BaseActivity {
     private final ActivityResultLauncher<String> requestNotificationPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
                 if (isGranted) {
-                    showDeadlineDialog();
+                    hasAlarmPermission();
                 } else {
                     Toast.makeText(this, R.string.toast_notification_permission_denied, Toast.LENGTH_SHORT).show();
                 }
@@ -59,6 +64,7 @@ public class EditActivity extends BaseActivity {
         EdgeToEdge.enable(this);
         setContentView(binding.getRoot());
 
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         savingRepository = new SavingRepository(this);
 
         setSupportActionBar(binding.toolbar);
@@ -97,9 +103,39 @@ public class EditActivity extends BaseActivity {
         });
     }
 
+    private void hasAlarmPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (alarmManager.canScheduleExactAlarms()) {
+                showDeadlineDialog();
+            } else {
+                showAlarmPermissionDialog();
+            }
+        } else {
+            showDeadlineDialog();
+        }
+    }
+
+    private void showAlarmPermissionDialog() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this)
+                .setTitle(R.string.dialog_title_request_alarm_permission)
+                .setMessage(R.string.dialog_message_request_alarm_permission)
+                .setIcon(R.drawable.ic_info)
+                .setNegativeButton(R.string.dialog_button_cancel, null)
+                .setPositiveButton(R.string.dialog_button_grant, (dialog, which) -> {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                        Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                        intent.setData(Uri.parse("package:" + getPackageName()));
+                        startActivity(intent);
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
     private void hasNotificationPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
-            showDeadlineDialog();
+            hasAlarmPermission();
         } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.POST_NOTIFICATIONS)) {
             Snackbar.make(binding.getRoot(), getString(R.string.snack_bar_permission_notifications), Snackbar.LENGTH_SHORT)
                     .setAction("Grant", v -> {
--- app/src/main/java/com/eipna/centsation/util/AlarmUtil.java
+++ app/src/main/java/com/eipna/centsation/util/AlarmUtil.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import com.eipna.centsation.data.saving.Saving;
 import com.eipna.centsation.receiver.DeadlineReceiver;
@@ -22,7 +23,13 @@ public class AlarmUtil {
 
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, saving.getID().hashCode(), intent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
         if (alarmManager != null) {
-            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, saving.getDeadline(), pendingIntent);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                if (alarmManager.canScheduleExactAlarms()) {
+                    alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, saving.getDeadline(), pendingIntent);
+                }
+            } else {
+                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, saving.getDeadline(), pendingIntent);
+            }
         }
     }
 
