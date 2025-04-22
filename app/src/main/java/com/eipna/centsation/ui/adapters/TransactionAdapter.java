59b79da95921be95d39ae2d61b36013db3a1054c
Eipna <Xirveipna@tuta.io>
2025-04-22 21:18:02 +0800

    Updated transaction adapter code and recycler layout

--- app/src/main/java/com/eipna/centsation/ui/adapters/TransactionAdapter.java
+++ app/src/main/java/com/eipna/centsation/ui/adapters/TransactionAdapter.java
@@ -10,21 +10,26 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.eipna.centsation.R;
+import com.eipna.centsation.data.Currency;
 import com.eipna.centsation.data.transaction.Transaction;
 import com.eipna.centsation.data.transaction.TransactionType;
 import com.eipna.centsation.util.DateUtil;
+import com.eipna.centsation.util.PreferenceUtil;
 import com.google.android.material.textview.MaterialTextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.ViewHolder> {
 
     private final Context context;
+    private final PreferenceUtil preferenceUtil;
     private final ArrayList<Transaction> transactions;
 
     public TransactionAdapter(Context context, ArrayList<Transaction> transactions) {
         this.context = context;
         this.transactions = transactions;
+        this.preferenceUtil = new PreferenceUtil(context);
     }
 
     @NonNull
@@ -37,7 +42,7 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
     @Override
     public void onBindViewHolder(@NonNull TransactionAdapter.ViewHolder holder, int position) {
         Transaction currentTransaction = transactions.get(position);
-        holder.bind(currentTransaction);
+        holder.bind(currentTransaction, preferenceUtil, context);
     }
 
     @Override
@@ -56,18 +61,19 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
             date = itemView.findViewById(R.id.transaction_date);
         }
 
-        public void bind(Transaction currentTransaction) {
+        public void bind(Transaction currentTransaction, PreferenceUtil preferenceUtil, Context context) {
+            String selectedCurrencySymbol = Currency.getSymbol(preferenceUtil.getCurrency());
+            double transactionAmount = Double.parseDouble(NumberFormat.getInstance().format(currentTransaction.getAmount()));
+
             type.setText(currentTransaction.getType());
             date.setText(DateUtil.getStringDate(currentTransaction.getDate()));
 
             if (currentTransaction.getType().equals(TransactionType.DEPOSIT.VALUE) || currentTransaction.getType().equals(TransactionType.CREATED.VALUE)) {
-                amount.setTextColor(itemView.getContext().getResources().getColor(R.color.md_theme_secondary, itemView.getContext().getTheme()));
-                amount.setText(String.format("%c%s", '+', NumberFormat.getInstance().format(currentTransaction.getAmount())));
-            }
-
-            if (currentTransaction.getType().equals(TransactionType.WITHDRAW.VALUE)) {
-                amount.setTextColor(itemView.getContext().getResources().getColor(R.color.md_theme_error, itemView.getContext().getTheme()));
-                amount.setText(String.format("%c%s", '-', NumberFormat.getInstance().format(currentTransaction.getAmount())));
+                amount.setTextColor(context.getResources().getColor(R.color.md_theme_secondary, itemView.getContext().getTheme()));
+                amount.setText(String.format(Locale.getDefault(), "%c%s%.2f", '+', selectedCurrencySymbol, transactionAmount));
+            } else if (currentTransaction.getType().equals(TransactionType.WITHDRAW.VALUE)) {
+                amount.setTextColor(context.getResources().getColor(R.color.md_theme_error, itemView.getContext().getTheme()));
+                amount.setText(String.format(Locale.getDefault(), "%c%s%.2f", '-', selectedCurrencySymbol, transactionAmount));
             }
         }
     }
